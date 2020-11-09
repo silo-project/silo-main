20750ef049a513b909880bda67330835e1889811
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-11-09 13:54:00 +0900

    NodeCreate() 에서 Node 초기화

--- src/node/node.c
+++ src/node/node.c
@@ -65,6 +65,10 @@ NODE * NodeCreate() {
 	
 	node_list[nodeid].nodeid   = nodeid;
 	node_list[nodeid].storage  = NULL;
+    node_list[nodeid].attribute= NULL;
+    node_list[nodeid].input    = NULL;
+    node_list[nodeid].output   = NULL;
+
 
 	return &node_list[nodeid];
 }
